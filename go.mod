ef3c220aade846c4c6e92e9317404f0aef86a403
Rick <rick@portto.com>
2023-12-06 21:38:10 +0800

    gorm

--- go.mod
+++ go.mod
@@ -5,11 +5,20 @@ go 1.19
 require (
 	github.com/natefinch/lumberjack v2.0.0+incompatible
 	go.uber.org/zap v1.26.0
+	gorm.io/driver/postgres v1.5.4
+	gorm.io/gorm v1.25.5
 )
 
 require (
 	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	go.uber.org/multierr v1.10.0 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
---
+++ internal/gorm/gorm.go
@@ -0,0 +1,43 @@
+package gorm
+
+import (
+	"fmt"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+	"gorm.io/gorm/schema"
+)
+
+func NewGormPostgresConn(config Config) (*gorm.DB, error) {
+	config = padDefault(config)
+	db, err := gorm.Open(
+		postgres.New(
+			postgres.Config{
+				DSN: config.DSN,
+			}),
+		&gorm.Config{
+			Logger: logger.Default.LogMode(logger.Warn),
+			NamingStrategy: schema.NamingStrategy{
+				SingularTable: config.SingularTable,
+			},
+			// In order to use the dialect translated errors(like ErrDuplicatedKey),
+			// enable the TranslateError flag when opening a db connection.
+			TranslateError: true,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("gorm.Open error: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("db.DB error: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(config.MaxIdleConns)
+	sqlDB.SetMaxOpenConns(config.MaxOpenConns)
+	sqlDB.SetConnMaxLifetime(config.ConnMaxLifetime)
+
+	return db, nil
+}
